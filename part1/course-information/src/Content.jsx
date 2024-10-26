60d9be414382fc8f7bdafc2128231063c4c11751
Digonto Shahniaz <ad9256@student.jamk.fi>
2024-10-26 09:14:06 +0300

    course-information 1.2 completed

--- part1/course-information/src/Content.jsx
+++ part1/course-information/src/Content.jsx
@@ -1,6 +1,20 @@
 
+import Part from './Part'
 
 const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
+    </>
+
+  )
+}
+
+export default Content
+
+/* const Content = (props) => {
   return (
     <>
       <p>{props.part1} {props.exercises1}</p>
@@ -11,4 +25,6 @@ const Content = (props) => {
   )
 }
 
-export default Content
+export default Content */
+
+
