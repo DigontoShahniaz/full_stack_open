cbade8b00aa2be68b42c73ddf7b4e53cf134ecf5
Digonto Shahniaz <ad9256@student.jamk.fi>
2024-10-29 09:16:53 +0200

    Exercise 1.10: Unicafe step 5 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 
+// statisticLine component
+const StatisticLine = (props) => {
+
+  return (
+    <p>{props.text}: {props.value}</p>
+  )
+}
+
+// statistics component
 const Statistics = (props) => {
   const total = props.good + props.neutral + props.bad
   const average = total > 0 ? (props.good - props.bad) / total : 0
@@ -9,12 +18,13 @@ const Statistics = (props) => {
     return (
       <div>
         <h2>Statistics</h2>
-        <p>good: {props.good}</p>
-        <p>neutral: {props.neutral}</p>
-        <p>bad: {props.bad}</p>
-        <p>all: {total}</p>
-        <p>average: {average}</p>
-        <p>positive: {positive}%</p>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={`${positive}%`} />
       </div>
     )
   }
@@ -26,17 +36,28 @@ const Statistics = (props) => {
   )
 }
 
+// button component
+const Button = (props) => {
+  return (
+    <button onClick={props.onSmash}>{props.text}</button>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onSmash={handleGood} text="good" />
+      <Button onSmash={handleNeutral} text="neutral" />
+      <Button onSmash={handleBad} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
 
