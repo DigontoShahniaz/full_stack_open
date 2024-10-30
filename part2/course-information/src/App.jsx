f26756a1d431db85149bc1bed88d6fc6b2324407
Digonto Shahniaz <ad9256@student.jamk.fi>
2024-10-30 14:02:51 +0200

    exercise 2.4: [200~

---
+++ part2/course-information/src/App.jsx
@@ -0,0 +1,59 @@
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Part = ({parts}) => parts.map((part => <p key={part.id}>{part.name} {part.exercises}</p>))
+
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part parts={parts} />
+    </div>
+  )
+}
+
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+    </>
+  )
+}
+
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+    
+  )
+
+}
+
+export default App
